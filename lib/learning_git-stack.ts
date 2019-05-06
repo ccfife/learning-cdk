e948debc86312349df444861657533c8cd2ddc6c
Chris Fife <ccfife@amazon.com>
2019-05-06 14:36:16 -0700

    updated to CDK v0.30.0 and copying static site content to s3

--- lib/learning_git-stack.ts
+++ lib/learning_git-stack.ts
@@ -1,16 +1,26 @@
 import cdk = require('@aws-cdk/cdk');
 import s3 = require('@aws-cdk/aws-s3');
+import s3deploy = require('@aws-cdk/aws-s3-deployment');
 
 export class LearningGitStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-  new s3.Bucket(this,'bucket',{
-      encryption: s3.BucketEncryption.KmsManaged
+  const bucket = new s3.Bucket(this,'bucket',{
+      //encryption: s3.BucketEncryption.KmsManaged,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'about-me.html'
     });
 
-    //bucket.grantPublicAccess();
+    bucket.grantPublicAccess();
+
+    new s3deploy.BucketDeployment(this, 'deployWebsite', {
+      source: s3deploy.Source.asset('./static_website/sonar-master'),
+      destinationBucket: bucket,
+      destinationKeyPrefix: 'web/static' 
+    });
+    
     //bucket.addLifecycleRule({abortIncompleteMultipartUploadAfterDays: 1});
 
        //to access low-level cfn resources
