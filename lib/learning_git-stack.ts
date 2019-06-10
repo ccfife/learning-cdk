49a0a917e471e1796696943bf35578c2c56295f9
Chris Fife <ccfife@amazon.com>
2019-06-10 16:17:41 -0700

    added cloudfront cdn in front of S3 bucket

--- lib/learning_git-stack.ts
+++ lib/learning_git-stack.ts
@@ -1,19 +1,18 @@
 import cdk = require('@aws-cdk/cdk');
 import s3 = require('@aws-cdk/aws-s3');
 import s3deploy = require('@aws-cdk/aws-s3-deployment');
+import cloudfront = require('@aws-cdk/aws-cloudfront');
+import iam = require('@aws-cdk/aws-iam');
 
 export class LearningGitStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // s3 bucket with support for website hosting
   const bucket = new s3.Bucket(this,'staticWebsite_v1',{
-      //encryption: s3.BucketEncryption.KmsManaged,
       websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'about-me.html'
+      websiteErrorDocument: '404.html'
     });
-
-    //bucket.grantPublicAccess();
 
     new s3deploy.BucketDeployment(this, 'deployWebsite', {
       source: s3deploy.Source.asset('static_website/sonar-master'),
@@ -21,11 +20,51 @@ export class LearningGitStack extends cdk.Stack {
       destinationKeyPrefix: 'web/static' 
     });
     
+    //restrict access to S3 bucket to CloudFront
+    // bucket.grantPublicAccess(); <- dont do this
+    const origin = new cloudfront.CfnCloudFrontOriginAccessIdentity(this, 'BucketOrigin', {
+      cloudFrontOriginAccessIdentityConfig: {
+        comment: 'sonar master'
+      }
+    });
+
+    bucket.grantRead(new iam.CanonicalUserPrincipal(
+      origin.cloudFrontOriginAccessIdentityS3CanonicalUserId
+    ));
+
+    //create cloudfront distribution
+    const cdn = new cloudfront.CloudFrontWebDistribution(this, 'cloudfront', {
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.AllowAll,
+      priceClass: cloudfront.PriceClass.PriceClassAll,
+      originConfigs: [
+        {
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+              maxTtlSeconds: undefined,
+              allowedMethods:
+                cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS
+            }
+          ],
+          originPath: 'web/static',
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentityId: origin.cloudFrontOriginAccessIdentityId
+          }
+        }
+      ]
+    })
+
     //bucket.addLifecycleRule({abortIncompleteMultipartUploadAfterDays: 1});
 
        //to access low-level cfn resources
        //const bucketResource = bucket.node.findChild('Resource') as s3.CfnBucket;
        //bucketResource.options.metadata = {createdByHighLevelAbsraction: 'False'};
 
+       //output cloudfront URL
+        new cdk.CfnOutput(this, 'CloudFrontURL', {
+          description: 'CDN URL',
+          value: "https://" + cdn.domainName
+        })
   }
 }
